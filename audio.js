30ad3725d6bf69a1a4d6e9c0863a6b0761a0880b
NidhiSingh6 <snidhi160802@gmail.com>
2024-05-30 11:28:48 +0530

    Audio Buttons

--- audio.js
+++ audio.js
@@ -16,21 +16,41 @@ const audio6=document.querySelector('#audio6')
 // console.log(audio1,audio2,audio3,audio4,audio5,audio6); 
 
 applause.addEventListener('click',()=>{
+    stopAudio()
     audio1.play()
 })
 boo.addEventListener('click',()=>{
+    stopAudio()
     audio2.play()
 })
 gasp.addEventListener('click',()=>{
+    stopAudio()
     audio3.play()
 })
 tada.addEventListener('click',()=>{
+    stopAudio()
     audio4.play()
 })
 victory.addEventListener('click',()=>{
+    stopAudio()
     audio5.play()
 })
 wrong.addEventListener('click',()=>{
+    stopAudio()
     audio6.play()
 })
 
+function stopAudio(){
+    audio1.pause()
+    audio1.currentTime=0
+    audio2.pause()
+    audio2.currentTime=0
+    audio3.pause()
+    audio3.currentTime=0
+    audio4.pause()
+    audio4.currentTime=0
+    audio5.pause()
+    audio5.currentTime=0
+    audio6.pause()
+    audio6.currentTime=0
+}
